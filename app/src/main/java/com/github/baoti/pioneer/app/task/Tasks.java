cdd3eec4153332cd6c895c4c2dc7a02c5388fc87
liuyedong <yed.liu@gmail.com>
2015-01-19 08:52:15 +0800

    为 SafeTask 添加注释

--- app/src/main/java/com/github/baoti/pioneer/app/task/Tasks.java
+++ app/src/main/java/com/github/baoti/pioneer/app/task/Tasks.java
@@ -8,14 +8,34 @@ import com.github.baoti.pioneer.app.task.compat.AsyncTask;
  * Created by liuyedong on 14-12-26.
  */
 public class Tasks {
+    /**
+     * 储存执行异常信息的 Task
+     * @param <Result>
+     */
     public interface SafeTask<Result> {
 
+        /**
+         * 是否正在运行, 已取消或失败或成功, 都返回 false
+         * @return
+         */
         boolean isRunning();
 
+        /**
+         * 是否有了执行结果, 正在运行时返回 null, 已取消时返回 null, 失败或成功返回 true.
+         * @return
+         */
         boolean hasResultOrException();
 
+        /**
+         * 得到失败时的异常, 未产生异常时返回 null.
+         * @return
+         */
         Exception getException();
 
+        /**
+         * 得到成功时的结果, 未得到结果时返回 null.
+         * @return
+         */
         Result getResult();
     }
 
