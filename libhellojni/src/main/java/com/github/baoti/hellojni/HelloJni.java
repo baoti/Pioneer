b0480df6d1f219cc578686c284c817eea746557b
baoti <yed.liu@gmail.com>
2015-09-04 19:19:09 +0800

    使用 com.android.model.library 插件建立模块

---
+++ libhellojni/src/main/java/com/github/baoti/hellojni/HelloJni.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014-2015 Sean Liu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.baoti.hellojni;
+
+public class HelloJni {
+
+    /* this is used to load the 'hello-jni' library on application
+     * startup. The library has already been unpacked into
+     * /data/data/com.github.baoti/lib/libhello-jni.so at
+     * installation time by the package manager.
+     */
+    static {
+        System.loadLibrary("hello-jni");
+    }
+
+    public native static String stringFromJNI();
+}
